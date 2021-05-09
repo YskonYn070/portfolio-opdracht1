9bf6186878b3ca3feaa1cf2486858218be906b0e
YskonYn070 <ilyas.laari@gmail.com>
2021-05-09 22:45:21 +0200

    training klasse aangemaakt

--- src/com/company/Training.java
+++ src/com/company/Training.java
@@ -1,5 +1,22 @@
 package com.company;
 
-public interface Training {
+      class Training  {
 
-}
+
+        public int maximaleLeeftijd;
+        public  int maximaleAfstand;
+
+        public Training(int maximaleLeeftijd, int maximaleAfstand)
+        {
+            this. maximaleLeeftijd = maximaleLeeftijd;
+            this. maximaleAfstand = maximaleAfstand;
+        }
+
+        public  boolean LeeftijdsCheckWeigering(int leeftijd, int afstand) {
+
+            return (leeftijd < maximaleLeeftijd) || (afstand < maximaleAfstand);
+        }
+    }
+
+
+
